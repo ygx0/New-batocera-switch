dc951bbc21f49cb4e2b613e0dfdf34d34451b0d4
foclabroc <foclabroc@hotmail.fr>
2026-01-22 16:11:00 +0100

    add updater

--- roms/ports/Switch Updater.sh
+++ roms/ports/Switch Updater.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-#####################################################################
-#                      PORTS: SWITCH UPDATER                        #
-#                  -----------------------------                    #
-#                 > https://discord.gg/SWBvBkmn9P                   #
-#              > github.com/foclabroc/batocera-switch                #
-#####################################################################
-updater=/userdata/system/switch/extra/batocera-switch-updater.sh
+
+updater=/userdata/system/switch/extra/appimage_updater.sh
 rm "$updater" 2>/dev/null 
-wget -q --no-check-certificate --no-cache --no-cookies -O "$updater" "https://raw.githubusercontent.com/foclabroc/batocera-switch/main/system/switch/extra/batocera-switch-updater.sh"
+wget -q --no-check-certificate --no-cache --no-cookies -O "$updater" "https://raw.githubusercontent.com/foclabroc/New-batocera-switch/refs/heads/main/system/switch/extra/appimage_updater.sh"
 dos2unix "$updater"
 chmod a+x "$updater"
-bash /userdata/system/switch/extra/batocera-switch-updater.sh
+DISPLAY=:0.0
+xterm -fullscreen -hold -bg black -fa "DejaVuSansMono" -fs 12 -en UTF-8 -e "bash /userdata/system/switch/extra/appimage_updater.sh"
