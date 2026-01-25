7e3a60ea26e1a63ae636ff73c5802b31b7bf45c9
foclabroc <foclabroc@hotmail.fr>
2026-01-25 09:47:50 +0100

    --+

--- system/switch/configgen/generators/edenGenerator.py
+++ system/switch/configgen/generators/edenGenerator.py
@@ -412,7 +412,7 @@ class EdenGenerator(Generator):
         # YUZU USER SAVE
         ensure_symlink(
             "/userdata/saves/switch/eden_citron/save/save_user",
-            "/userdata/system/configs/yuzu/nand/user"
+            "/userdata/system/configs/yuzu/nand/user/save"
         )
         # YUZU SYSTEM SAVE
         ensure_symlink(
